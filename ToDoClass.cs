284335f1ca9ba11ef3fcf59d68f9abf5bf9e2ee0
Slotin97 <adeyeyesodiq67@gmail.com>
2023-02-15 20:35:00 -0500

    Create ToDoClass.cs

---
+++ ToDoClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todolistapplication
+{
+    public class TodoItems
+    {
+        public string title;
+        public string description;
+        public DateTime dueDate;
+
+        public TodoItems(string titles, string descriptions, DateTime dueDate)
+        {
+            this.title = titles;
+            this.description = descriptions;
+            this.dueDate = dueDate;
+        }
+
+    }
+    
+}
